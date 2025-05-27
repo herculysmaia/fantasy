7c7403efe55ca12f84f9e8cc4168801ef97481be
Herculys S Maia <herculysmaia@live.com>
2025-05-27 09:10:09 -0300

    Atualiza a exibição de movimentações financeiras com novos saldos e melhora o layout dos times

--- src/app/screen/finance/inicio.rs
+++ src/app/screen/finance/inicio.rs
@@ -1,7 +1,8 @@
-use iced::{Color, Font, Length, Task};
+use iced::{Alignment, Color, Font, Length, Task};
 use iced::widget::{column, row, text, Image, Space};
 use iced::widget::image::Handle;
 
+use crate::app::db::TipoMovimentacao;
 use crate::app::screen::finance::TimeData;
 use crate::app::{obter_times, MessageDispatcher, Screen, ScreenTaskReturn, Time};
 
@@ -96,30 +97,50 @@ fn widget_time(time: &Time) -> iced::Element<'static, MessageDispatcher> {
         weight: iced::font::Weight::Bold,
         ..Default::default()
     };
-
-    let mut lista = column![];
+    
+    let mut saldo_deposito: f32 = 0.0;
+    let mut saldo_participacao: f32 = 0.0;
+    let mut saldo_premiacao: f32 = 0.0;
 
     for movimentacao in time.financeiro.obter_movimentacoes() {
-        lista = lista.push(
-            row![
-                text("Depósitos")
-                    .font(segoe_ui)
-                    .size(12),
-                Space::with_width(Length::Fill),
-                text(format!("R$ {:.2}", movimentacao.valor as f32 / 100.0))
-                    .font(segoe_ui)
-                    .size(12),
-                ]
-        )
+        match movimentacao.tipo {
+            TipoMovimentacao::Participacao =>  saldo_participacao += movimentacao.valor as f32 / 100.0,
+            TipoMovimentacao::Premiacao => saldo_premiacao += movimentacao.valor as f32 / 100.0,
+            TipoMovimentacao::Deposito => saldo_deposito += movimentacao.valor as f32 / 100.0,
+            _ => {}
+        }
     }
 
+    let lista = column![
+        row![
+            text("Depósitos").font(segoe_ui).size(15),
+            Space::with_width(Length::Fill),
+            text("R$").font(segoe_ui).size(15),
+            text(format!("{:.0}", saldo_deposito)).align_x(Alignment::End).width(30).font(segoe_ui).size(15),
+        ],
+        Space::with_height(Length::Fill),
+        row![
+            text("Participações").font(segoe_ui).size(15),
+            Space::with_width(Length::Fill),
+            text("R$").font(segoe_ui).size(15),
+            text(format!("{:.0}", saldo_participacao)).align_x(Alignment::End).width(30).font(segoe_ui).size(15),
+        ],
+        Space::with_height(Length::Fill),
+        row![
+            text("Premiações").font(segoe_ui).size(15),
+            Space::with_width(Length::Fill),
+            text("R$").font(segoe_ui).size(15),
+            text(format!("{:.0}", saldo_premiacao)).align_x(Alignment::End).width(30).font(segoe_ui).size(15),
+        ],
+    ];
+        
     column![
         row![
-            Image::new(Handle::from_bytes(time.escudo_png.clone())).height(50),
+            Image::new(Handle::from_bytes(time.escudo_png.clone())).height(60),
             column![
-                text(format!("{}", time.nome_do_time)).font(segoe_ui_bold),
+                text(format!("{}", time.nome_do_time)).font(segoe_ui_bold).size(20),
                 row![
-                    text(format!("R$ {:.2}", time.financeiro.obter_saldo() as f32)).font(segoe_ui_bold).size(20),
+                    text(format!("R$ {:.0}", time.financeiro.obter_saldo() as f32)).font(segoe_ui_bold).size(30),
                     Space::with_width(Length::Fill),
                     text(format!("{}", time.nome_do_dono)).font(segoe_ui).size(10),
                 ].width(Length::Fill)
